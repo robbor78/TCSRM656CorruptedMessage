9991a14da35addf7e129ed5d647b43064db66646
username <email@email.com>
2016-03-03 12:22:07 +0200

    started work

--- SRM656CorruptedMessage/Program.cs
+++ SRM656CorruptedMessage/Program.cs
@@ -14,7 +14,29 @@ namespace SRM656CorruptedMessage
 
     public String reconstructMessage(String s, int k)
     {
-      return null;
+      if (k == 0) { return s; }
+
+      int maxCount = 0;
+      char maxC = '.';
+      Dictionary<char, int> counts = new Dictionary<char, int>();
+      int length = s.Length;
+      for (int i = 0; i < length; i++)
+      {
+        char c = s[i];
+        if (!counts.ContainsKey(c))
+        {
+          counts.Add(c, 0);
+        }
+        counts[c]++;
+        if (counts[c] > maxCount)
+        {
+          maxCount = counts[c];
+          maxC = c;
+        }
+      }
+
+      return new string(maxC, length);
+
     }
   }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SRM656CorruptedMessage;
 
 namespace UnitTestProject1
 {
@@ -9,6 +10,22 @@ namespace UnitTestProject1
     [TestMethod]
     public void TestMethod1()
     {
+
+      string[] allMessages = new string[] { "hello", "abc" };
+      int[] allK = new int[] { 3,3 };
+      string[] allCorrected = new string[] { "lllll","ddd" };
+
+      int length = allMessages.Length;
+      for (int i = 0; i < length; i++)
+      {
+        Program p = new Program();
+
+        string message = allMessages[i];
+        int k = allK[i];
+        string actual = p.reconstructMessage(message, k);
+        string expected = allCorrected[i];
+        Assert.AreEqual(expected, actual);
+      }
     }
   }
 }
