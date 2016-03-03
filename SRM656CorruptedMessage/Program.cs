5ec7bca8ee786ba3a0a13720189bb68581ba4347
username <email@email.com>
2016-03-03 11:51:43 +0200

    start of project

---
+++ SRM656CorruptedMessage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRM656CorruptedMessage
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public String reconstructMessage(String s, int k)
+    {
+      return null;
+    }
+  }
+}
